9701e2f1326c1320232a5b0962dcf4f5a9d9a5e5
hakluke <13975395+hakluke@users.noreply.github.com>
2020-01-19 19:19:45 +1000

    updated version

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -28,7 +28,7 @@ type Config struct {
 func NewConfig() Config {
 	var conf Config
 	// default values
-	conf.Version = "beta 4" 
+	conf.Version = "beta 5" 
 	conf.DisplayVersion = false
 	conf.Url = ""
 	conf.Depth = 1
