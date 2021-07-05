f86c5e1123e65ec0e44e756c0f0ebd099beb938f
hakluke <13975395+hakluke@users.noreply.github.com>
2021-07-05 15:25:49 +1000

    complete rewrite

--- go.mod
+++ go.mod
@@ -1,22 +1,16 @@
 module github.com/hakluke/hakrawler
 
-go 1.12
+go 1.16
 
 require (
-	github.com/PuerkitoBio/goquery v1.5.0 // indirect
-	github.com/antchfx/htmlquery v1.2.1 // indirect
-	github.com/antchfx/xmlquery v1.2.2 // indirect
-	github.com/antchfx/xpath v1.1.3 // indirect
+	github.com/PuerkitoBio/goquery v1.7.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.6 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
-	github.com/golang/groupcache v0.0.0-20191227052852-215e87163ea7 // indirect
-	github.com/google/uuid v1.1.1
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
-	github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	google.golang.org/appengine v1.6.5 // indirect
-	gopkg.in/h2non/gock.v1 v1.0.15
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
---
+++ hakrawler.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	threads := flag.Int("t", 8, "Number of threads to utilise. Default is 8.")
+	depth := flag.Int("d", 2, "Depth to crawl. Default is 2.")
+	insecure := flag.Bool("insecure", false, "Disable TLS verification.")
+	flag.Parse()
+
+	// get each line of stdin, push it to the work channel
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		crawl(s.Text(), *threads, *depth, *insecure)
+	}
+}
+
+func crawl(url string, threads int, depth int, insecure bool) {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// MaxDepth is 2, so only the links on the scraped page
+		// and links on those pages are visited
+		colly.MaxDepth(2),
+		colly.Async(true),
+	)
+
+	// Set parallelism
+	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: threads})
+
+	// Print every href found, and visit it
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		absoluteURL := e.Request.AbsoluteURL(link)
+
+		if absoluteURL != "" {
+			fmt.Printf("[href] %s\n", e.Request.AbsoluteURL(link))
+			// Visit link found on page on a new thread
+			e.Request.Visit(link)
+		}
+	})
+
+	// find and print all the JavaScript files
+	c.OnHTML("script[src]", func(e *colly.HTMLElement) {
+		link := e.Attr("src")
+		absoluteURL := e.Request.AbsoluteURL(link)
+		if absoluteURL != "" {
+			fmt.Printf("[script] %s\n", e.Request.AbsoluteURL(link))
+		}
+	})
+
+	// find and print all the JavaScript files
+	c.OnHTML("form[action]", func(e *colly.HTMLElement) {
+		link := e.Attr("action")
+		absoluteURL := e.Request.AbsoluteURL(link)
+		if absoluteURL != "" {
+			fmt.Printf("[form] %s\n", e.Request.AbsoluteURL(link))
+		}
+	})
+
+	// Skip TLS verification if -insecure flag is present
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+	})
+
+	// Start scraping
+	c.Visit(url)
+	// Wait until threads are finished
+	c.Wait()
+}
